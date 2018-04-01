5940529d6fc695a7199a5554d1f31a6779c077b6
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-04-01 18:45:42 -0400

    got my java and css to work properly. sometimes a warning comes up in my console log thats says warning reset.css something or another. not sure what kinda sorcery it is.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,28 @@
 
- $("Pic1").on("click", ".rock", function() {
+var num = [1, 5, 10, 20]; 
 
-   
-    alert("You clicked a shiny rock!");
+var numRan = num[Math.round(Math.random())];
 
-  });
-
-
+var goalScore = 75;
+ $("#score").text(goalScore);
 
 
+var counter = 0;
+$("#rocks").on("click", ".Pic1", function() {
+
+counter += numRan;
+    alert("Your new score is " + counter)
+
+    if (counter === goalScore) {
+      
+
+            alert("You win!");
+          }
+          else if (counter >= goalScore) {
+
+            alert("You lose!!");
+          }
+      
+       
+      
+});
