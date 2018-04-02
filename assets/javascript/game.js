f5faead372cad798dfc2942c7d4e4284cb1b9ffb
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-04-01 21:29:03 -0400

    i added some code from an lesson fixed all my errors and now im getting a NaN error cant seem to find it.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,54 @@
+var num = (4, 5, 11, 20);
 
-var num = [1, 5, 10, 20]; 
-
-var numRan = num[Math.round(Math.random())];
+var magicRocks = $("#rocks");
+var numRan = num[Math.round(Math.random()+ 4)];
 
 var goalScore = 75;
- $("#score").text(goalScore);
+$("#score").text(goalScore);
 
+for (var i = 0; i < num.length; i++) {
+
+    var shineyRocks = $("<img>");
+
+
+    shineyRocks.addClass("rocks");
+
+
+    shineyRocks.attr("src", "assets/images/download.jpg", "assets/images/2013-10-Blue-Crystal-Abstract.jpg", "assets/images/safe_image_1_dfd63a86-36a6-4376-98c2-fc4f62258150_1024x1024.jpg", "assets/images/stock-vector-purple-crystal-cluster-vector-design-576295027.jpg");
+
+
+    shineyRocks.attr("data-rocksvalue", num[i]);
+
+
+    magicRocks.append(shineyRocks);
+};
+
+
+// magicRocks.on("click", ".shineyRocks", function () {
+
+
+//     var rocks = ($(this).attr("data-rocksvalue"));
+//     rocks = parseInt(rocks);
+
+//     counter += shineyRocks;
+
+
+//     alert("New score: " + counter);
+// });
 
 var counter = 0;
-$("#rocks").on("click", ".Pic1", function() {
+$("#rocks").on("click", ".Pic1", function () {
 
-counter += numRan;
-    alert("Your new score is " + counter)
+    counter += numRan;
+    alert("Your new score is " + counter);
 
     if (counter === goalScore) {
-      
 
-            alert("You win!");
-          }
-          else if (counter >= goalScore) {
 
-            alert("You lose!!");
-          }
-      
-       
-      
+        alert("You win!");
+    } else if (counter >= goalScore) {
+
+        alert("You lose!!");
+    }
+
 });
